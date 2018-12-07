fa17f94a2d1b5652bf1b50768915a2f4e51740c8
Sinisa Manojlovic <sinisamanojlovic1@gmail.com>
2018-12-07 18:40:43 +0100

    Zadatak 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import TriviaApp from './components/TriviaApp.vue'
+
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', redirect: '/trivia'},
+  { path: '/trivia', component: TriviaApp, name: 'trivia'}
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
